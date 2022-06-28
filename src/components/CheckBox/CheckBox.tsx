753a3390c1ef02f506c7b85cc5d028faa3e8bbf4
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-06-28 11:55:25 +0900

    CheckBoxコンポーネントのコーディング

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,5 +1,123 @@
 import React from "react";
+import "./CheckBox.scss";
 
 export const CheckBox: React.VFC = () => {
-  return <div>CheckBox</div>;
+  return (
+    <ul className="checkbox">
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+      <li>
+        <label htmlFor="tokyo">
+          <input type="checkbox" name="" id="tokyo" />
+          <span>東京都</span>
+        </label>
+      </li>
+    </ul>
+  );
 };
