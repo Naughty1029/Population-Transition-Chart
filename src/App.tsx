787a1bcceb6067a40611dab7156f9b69d87a0876
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-06-28 13:57:45 +0900

    usequeryで都道府県リスト取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Layout } from "./components/Layout/Layout";
 
+const queryClient = new QueryClient();
+
 const App: React.VFC = () => {
-  return <Layout />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
+  );
 };
 export default App;
--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,123 +1,40 @@
 import React from "react";
+import { useQuery } from "react-query";
 import "./CheckBox.scss";
 
 export const CheckBox: React.VFC = () => {
+  const url = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+  const fetchPrefectures = async () => {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-api-key": "KKigZnHRZ261R2CQ4EuI5dJOpDtwRH3oT1V62ahn",
+      },
+    });
+    return res.json();
+  };
+
+  const { data, isLoading } = useQuery("prefectures", fetchPrefectures);
+  console.log(data);
+
+  type Props = {
+    prefCode: number;
+    prefName: string;
+  };
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
   return (
     <ul className="checkbox">
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
-      <li>
-        <label htmlFor="tokyo">
-          <input type="checkbox" name="" id="tokyo" />
-          <span>東京都</span>
-        </label>
-      </li>
+      {data["result"].map((pref: Props) => (
+        <li key={pref.prefCode}>
+          <label htmlFor={pref.prefName}>
+            <input type="checkbox" name="" id={pref.prefName} />
+            <span>{pref.prefName}</span>
+          </label>
+        </li>
+      ))}
     </ul>
   );
 };
